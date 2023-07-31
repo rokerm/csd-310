9ae0b8243f3ec40dc55d9f544f8e9e4aba60fefa
Malachi Roker <mjrok10@gmail.com>
2023-07-30 19:59:34 -0500

    modified:   module_9/pysports_update_and_delete.py

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -30,10 +30,61 @@ def show_players(cursor, title):
 
 try:
 
-    # connect to the pysports database 
+    #connect to the pysports database 
     db = mysql.connector.connect(**config)
 
     cursor = db.cursor()
 
     #insert new player
-    add_player = ("INSERT INTO player(first_name, last_name )")
+    new_player = ("INSERT INTO player(first_name, last_name, team_id)" "VALUES (%s, %s, %s)")
+
+    #player information
+    player_info = ("Smeagol", "Shire Folk", 1)
+
+    #insert new record
+    cursor.execute(new_player, player_info)
+
+    #commit new record in database
+    db.commit()
+
+    #shows all players in player table after insert query
+    show_players(cursor, " Displaying Players After Insert ")
+
+    #update new inserted record
+    update_player = ("UPDATE player SET team_id =2, first_name = 'Gollum', last_name = 'Ring Stealer' WHERE first_name = 'Smeagol'")
+
+    cursor.execute(update_player)
+
+    #shows all players after update query
+    show_players(cursor, " Displaying Players After UPDATE ")
+
+    #delete query
+    delete_player = ("DELETE FROM player WHERE first_name = 'Gollum'")
+
+    cursor.execute(delete_player)
+
+    #shows all players after delete query
+    show_players(cursor, " Displaying Players After Delete ")
+
+    input ("\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    #disconnects from MYSQL
+    db.close()
+
+
+
+
+
